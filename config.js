79f734dc0d2e9b62fc05f2140d7038a210f97350
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-10-24 08:09:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-exports.master = '466672305989222415';
-exports.token = 'NjAwODI5OTc0MzE5OTg4NzM3.XbGKhg.KD_KL8a7MscbhyluaaN4NZdJUiE';
-exports.ytkey = 'AIzaSyBHdVZL0ngJ66SUNakaYol1i3rRltTfmE4';
 
-/* IDs for bot's test server. This allows users from guilds to report and request features. */
-exports.masterServer = 'YOUR_MASTER_BOT_SERVER_ID_HERE';
-exports.reportChannel = 'YOUR_MASTER_BOT_SERVER_REPORT_CHANNEL_ID_HERE';
-exports.requestChannel = 'YOUR_MASTER_BOT_SERVER_REQUEST_CHANNEL_ID_HERE';
-exports.feedbackChannel = 'YOUR_MASTER_BOT_SERVER_FEEDBACK_CHANNEL_ID_HERE';
-exports.dmChannel = 'YOUR_MASTER_BOT_SERVER_DM_CHANNEL_ID_HERE';
