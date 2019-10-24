6cc2ed00f6bd7664b5b9945b384f94ebcbbe50c4
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-10-24 08:52:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ KlasaClient.defaultPermissionLevels
 
 const client = new KlasaClient({
   ownerID : master,
-  prefix: 'db!',
+  prefix: '+',
   cmdDeleting: true,
   cmdEditing: true,
   cmdLogging: true,
